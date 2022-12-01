9b201548b7f5a3d2620cb0adc8fd74307cb6c3f6
josetrivino89 <josetrivino89@gmail.com>
2022-12-01 00:27:37 -0300

    se termianron los metodos create,update,get, delete y buscar

--- controller/productosControler.js
+++ controller/productosControler.js
@@ -39,14 +39,68 @@ const CreateProductos=async(req, res)=>{
     }
 }
 //actualizar los productos y mandarlos a la Base:
-const upDateProductos=(req, res)=>{}
-
+const upDateProductos= async (req, res)=>{
+    //1-hacemos la reestructuracion de lo que va en la ruta que sera dinamica:
+    const{id}=req.params
+    //2- accedemos al body:
+    const productoData = req.body
+    //3-creamos un try-catch para crear una respuesta cuando todo esta bien:
+    try {
+        const productoDB = await ProductosModel.findById(id)
+        //buscamos el producto en la BD con el metodo findByID de moongose
+        await ProductosModel.findByIdAndUpdate(id,productoData)
+        res.status(200).send({msg:"Producto actualizado correctamente"})
+       
+        
+    } catch (error) {
+        res.status(400).send({msg:"Producto  no pudo ser actualizado"})
+        
+    }
+}
 //borrar los productos:
-const deleteProductos=(req, res)=>{}
+const deleteProductos= async(req, res)=>{
+    //1-hacemos la reestructuracion de lo que va en la ruta que sera dinamica:
+    const{id}=req.params
+    //2-creamos un try-catch para crear una respuesta cuando todo esta bien:
+    try {
+    //3- buscamos el producto por el ID de que le asigna mongoDB, y usamos el metodo findByIdAndDelete de moongose, aqui no hace falta la constante porque no queremos que nos devuelva el producto eliminado
+        await ProductosModel.findByIdAndDelete(id)
+        //hacemos un res del producto buscado para que nos lo muestre          
+        res.status(200).send({msg:"Producto eliminado correctamente"})
+       
+        
+    } catch (error) {
+        //hacemos un res por si no encuentra el producto por la ID
+        res.status(400).send({msg:"Producto no encontrado"})
+        
+    }
+
+}
+
+//buscar los productos:
+const buscarProductos=async(req, res)=>{
+        //1-hacemos la reestructuracion de lo que va en la ruta que sera dinamica:
+        const{id}=req.params
+        //2-creamos un try-catch para crear una respuesta cuando todo esta bien:
+        try {
+        //3- creamos una cosntante y buscamos el producto por el ID de que le asigna mongoDB, es importante crear la constante para luego pasarle
+            const productoBuscado = await ProductosModel.findById(id)
+            //hacemos un res del producto buscado para que nos lo muestre          
+            res.status(200).send(productoBuscado)
+           
+            
+        } catch (error) {
+            //hacemos un res por si no encuentra el producto por la ID
+            res.status(400).send({msg:"Producto no encontrado"})
+            
+        }
+
+}
 
 module.exports={
     getProductos,
     CreateProductos,
     upDateProductos,
-    deleteProductos
+    deleteProductos,
+    buscarProductos
 }
--- routes/producto.js
+++ routes/producto.js
@@ -10,6 +10,8 @@ router.post('/',ProductosController.CreateProductos)
 //recurso para editar un producto
 router.put('/:id',ProductosController.upDateProductos)//aqui le agregamos el id para poder buscarlo y actualizarlo
 //recurso para borrar un producto
-router.delete('/',ProductosController.deleteProductos)
+router.delete('/:id',ProductosController.deleteProductos)
+//recurso para buscar un producto
+router.get('/:id',ProductosController.buscarProductos)//aqui le agregamos el id para poder buscarlo
 
 module.exports=router;
